7420169b05ba73b21a14fc1ab438029a375f2929
Bri Lance <bri@inleague.org>
2016-06-23 15:51:17 -0400

    test 1 framework files

--- tile.js
+++ tile.js
@@ -1,14 +1,21 @@
 function Tile(opts){
-	this.id = opts.id;
 	this.img = opts.img;
 	//stylesheet to specify absolute positioning
 	this.imgTmpl = "<img src='" + this.img + "' style='top:{1}px;left:{2}px;'/>";
+	this.width = 10;
+	this.height = 10;
 }
 
 Tile.prototype = {
 	draw: function(document, ycoord, xcoord){
 		var tmpl = this.imgTmpl.replace('{1}', ycoord);
 		tmpl = tmpl.replace('{2}', xcoord);
-		document.write(this.imgTmpl);
+		document.write(tmpl);
+	},
+	drawTest: function(document, ycoord, xcoord){
+		var tmpl = "<div class='tile' style='top:{1}px;left:{2}px;'></div>";
+		tmpl = this.imgTmpl.replace('{1}', ycoord);
+		tmpl = tmpl.replace('{2}', xcoord);
+		document.write(tmpl);
 	}
 };
